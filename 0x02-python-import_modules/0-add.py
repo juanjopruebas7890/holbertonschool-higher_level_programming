fc80fa018dc50ce8bdf901af3b0a36c8247e7d05
Juan Jose <1561@holbertonschool.com>
2020-04-30 17:26:11 +0000

    [fix] all code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 
-import add_0
-print(a, "+", b, "=", add_0.add(a, b))
+    print("{} + {} = {}".format(a, b, add(a, b)))
