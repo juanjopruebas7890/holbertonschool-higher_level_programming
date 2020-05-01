7fe782de984e46cfbee0adc32fb7dcee4adfccc5
Juan Jose <1561@holbertonschool.com>
2020-05-01 00:24:41 +0000

    [fix] new line & double qutes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,22 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    if (len(sys.argv) - 1 != 3):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+    if ((len(sys.argv) - 1) != 3):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if (sys.argv[2] == '+'):
+        if (sys.argv[2] == "+"):
             res = add(a, b)
             print("{} + {} = {}".format(a, b, res))
-        elif (sys.argv[2] == '-'):
+        elif (sys.argv[2] == "-"):
             res = sub(a, b)
             print("{} - {} = {}".format(a, b, res))
-        elif (sys.argv[2] == '*'):
+        elif (sys.argv[2] == "*"):
             res = mul(a, b)
             print("{} * {} = {}".format(a, b, res))
-        elif (sys.argv[2] == '/'):
+        elif (sys.argv[2] == "/"):
             res = div(a, b)
             print("{} / {} = {}".format(a, b, res))
         else:
