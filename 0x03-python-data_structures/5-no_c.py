b93b39f6aef7b2d1afbf9353ac887b091a55231c
Juan Jose <1561@holbertonschool.com>
2020-05-05 21:48:19 +0000

    [add] first version

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+        f = ''
+        for i in my_string:
+                if i == 'c' or i == 'C':
+                        continue
+                else:
+                        f = f + i
+        return (f)
