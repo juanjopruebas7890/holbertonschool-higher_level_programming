6590336566b127951d1d718da30269cb377b03dc
Juan Jose <1561@holbertonschool.com>
2020-05-20 04:06:19 +0000

    [add] first version

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Define class square """
+
+
+class Square:
+    """ Define class
+
+    Attributes:
+    __size: size of the square
+    """
+    def __init__(self, size=0):
+        """ Square initialized
+
+        Arguments:
+        size: size fo the sqr
+        return: none
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ will get the size
+
+        return: size of sqr
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ setter of size
+
+        s = size os sqr
+        """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ method to find area
+        return: area of sqr
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for f in range(self.__size):
+                for c in range(self.__size):
+                    print('#', end='')
+                print()
