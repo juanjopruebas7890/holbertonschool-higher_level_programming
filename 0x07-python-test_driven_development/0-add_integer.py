7a3d3ee6f2b8af7a95bb2f660515c8c0ddfa4bf4
Juan Jose <1561@holbertonschool.com>
2020-05-22 15:29:21 +0000

    [add] docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@ and returns the addition
 
 
 def add_integer(a, b=98):
+    """
+    Will add a + b
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
