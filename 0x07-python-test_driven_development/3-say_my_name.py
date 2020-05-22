2ef8cba490a479ed09e7fce8acf81fa4c32aa55a
Juan Jose <1561@holbertonschool.com>
2020-05-22 22:17:33 +0000

    [fix] docstring

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,15 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Arguments:
+
+    First_name: first name
+
+    last_name: last name
+
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
