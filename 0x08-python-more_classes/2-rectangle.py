4ee208af8f6439a0007895f6b64cfae9cb3601e3
Juan Jose <1561@holbertonschool.com>
2020-05-25 20:30:54 +0000

    [add] first version

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Creating a rectangle """
+
+
+class Rectangle:
+    """ define class """
+    def __init__(self, width=0, height=0):
+        """ inititialize variables """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ will get the widith
+        return: the widith
+        """
+        return (self.__width)
+
+    @property
+    def height(self, value):
+        """ will get the height
+        return: the height
+        """
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        """ setter of the size """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        """ setter of height """
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """ method to find area """
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """ method to find perm """
+        return (self.__height + self.__height + self.__width + self.__width)
