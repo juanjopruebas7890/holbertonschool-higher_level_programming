1d37d3f428a1aa86d8d9bab3fda2dacceedd87d3
Juan Jose <1561@holbertonschool.com>
2020-06-03 21:57:50 +0000

    [remove] name of file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ will read a file """
-    with open("my_file_0.txt", encoding="utf-8") as file:
+    with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line, end="")
