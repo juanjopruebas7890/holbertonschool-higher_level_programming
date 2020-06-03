04f940ca73c4b824974ea51549b06559e260d3b1
Juan Jose <1561@holbertonschool.com>
2020-06-03 21:59:09 +0000

    [fix] filename

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,7 +5,7 @@
 def number_of_lines(filename=""):
     """ will give number of lines """
     count = 0
-    with open("my_file_0.txt", mode="r", encoding="utf-8") as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         for line in file:
             count += 1
         return (count)
