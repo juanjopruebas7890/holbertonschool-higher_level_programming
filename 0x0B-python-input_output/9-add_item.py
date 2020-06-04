b4f734b7511f36b54cd44b35ac4b279d231b0026
Juan Jose <1561@holbertonschool.com>
2020-06-04 02:18:34 +0000

    [Add]

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" adds all arguments to a list """
+import sys from argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    myList = load_from_json_file(filename)
+except:
+    myList = []
+
+for arguments in argv[1]:
+    myList.append(arguments)
+
+save_to_json_file(myList, filename)
