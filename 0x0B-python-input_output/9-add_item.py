37c4c9605fea34e4d65b910e21fd2359a3a11054
Juan Jose <1561@holbertonschool.com>
2020-06-04 03:55:39 +0000

    [fix] syntax import

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ adds all arguments to a list """
-import sys from argv
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
