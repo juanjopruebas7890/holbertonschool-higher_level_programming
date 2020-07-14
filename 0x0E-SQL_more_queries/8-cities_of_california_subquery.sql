9ef08883c735b2b0775894109004c8e62e739642
Juan Jose <1561@holbertonschool.com>
2020-07-14 22:09:51 +0000

    [fix] uppercases

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list all the cities
 -- list all the cities
-SELECT id, name FROM cities where state_id = (
+SELECT id, name FROM cities WHERE state_id = (
        SELECT id FROM states WHERE name = "California")
 ORDER BY id ASC;
