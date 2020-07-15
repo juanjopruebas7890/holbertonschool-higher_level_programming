b46308ed2fa12855e75ca3a1ac9ac644ddf47521
Juan Jose <1561@holbertonschool.com>
2020-07-15 00:15:42 +0000

    [add]

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- list of dexter
+SELECT tv_genres.name FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows ON tv_show_genres.sow_id = tv_shows.id WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
