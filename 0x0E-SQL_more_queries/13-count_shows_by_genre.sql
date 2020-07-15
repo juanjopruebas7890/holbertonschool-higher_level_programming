defe285806f538528cfc171af71e262e128f234b
Juan Jose <1561@holbertonschool.com>
2020-07-15 00:03:46 +0000

    [fix]

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- list all genres
-SELECT tv_genres.name, COUNT(*) number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name, genre COUNT(*) number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
