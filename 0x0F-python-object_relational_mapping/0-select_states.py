96d2ef41393fdaa9ca8dd564142029afd04e3442
Juan Jose <1561@holbertonschool.com>
2020-08-09 00:54:59 +0000

    [add]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-""" Get state """
+"""
+Script will display states sorted by id in ascending order
+"""
 
-import sys
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main_":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == '__main__':
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states.id ORDER BY id ASC")
-    rows = cursor.fetchall()
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
