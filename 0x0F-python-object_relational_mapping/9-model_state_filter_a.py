bc0750a47661449964c9bfa996c29c281ea48e8e
Juan Jose <1561@holbertonschool.com>
2020-08-09 23:49:20 +0000

    [add] first

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  prints the first State object from the database """
+
+import sqlalchemy
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                        (user, password, database), pool_pre_ping=True)
+
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    states = sess.query(State).order_by(State.id.asc()).all()
+    for state in states:
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+    sess.close()
