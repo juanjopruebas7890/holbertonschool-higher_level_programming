acfeb764ac7df5755f7e861b1955c423361b64c3
Juan Jose <1561@holbertonschool.com>
2020-08-10 01:33:12 +0000

    [add] first

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Delte an object """
+
+import sqlalchemy
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                        (user, password, database), pool_pre_ping=True)
+
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    del = sess.query(State).order_by(State.id).all()
+    for row in del:
+        if 'a' in row.name:
+            sess.delete(row)
+    sess.commit()
+    sess.close()
