f2fd9dde85ff40394db32d23820192b190d81117
Juan Jose <1561@holbertonschool.com>
2020-08-18 21:58:34 +0000

    [fix] syntax

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that sends a POST request
-curl -X POST -sd "email=hr@holbertonschool.com&subject=I will always be here fo\
+curl -X POST -sd "email=hr@holbertonschool.com&subject=I will always be here fo
 r PLD" "$1"
