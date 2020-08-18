cff04c020dc1461299fe36e0bf11a43a3ffde063
Juan Jose <1561@holbertonschool.com>
2020-08-18 18:14:45 +0000

    [fix] typo

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a request to a url                                  
-curl -sI "$1" | grep "Content-Lenght:" | cut -f2 -d ' '
+curl -sI "$1" | grep "Content-Length:" | cut -f2 -d ' '
