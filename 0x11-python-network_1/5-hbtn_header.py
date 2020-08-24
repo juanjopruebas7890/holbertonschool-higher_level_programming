182f2872256798684cf9267bb2343a862724a3d1
Juan Jose <1561@holbertonschool.com>
2020-08-24 19:48:00 +0000

    [add] first version

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" takes a url and sends request with requests package """
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1], auth=('user', 'pass'))
+    print(r.headers['X-Request-Id'])
